ed37fbe4e7ffd9d43ae0459f3d7ebdd0dc8addc5
Ivan Zhao <ivanzhao8@gmail.com>
2023-04-28 21:24:22 -0400

    pushes build to prod

---
+++ example/build/precache-manifest.2da71d2477fe3e002243d6eab849e3e4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f3c0db5de9be0e561a6b5ae6b59c30af",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c784b77ccc74d4b84756",
+    "url": "./static/css/main.1c0dbb44.chunk.css"
+  },
+  {
+    "revision": "223232e48675d2b0b2c8",
+    "url": "./static/js/2.92f7eca6.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.92f7eca6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c784b77ccc74d4b84756",
+    "url": "./static/js/main.e1f2c093.chunk.js"
+  },
+  {
+    "revision": "c75943980549a5730fed",
+    "url": "./static/js/runtime-main.acc15064.js"
+  }
+]);
